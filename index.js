81a203e78d8837e20870cd06133fb9f0696129c8
Erdem Arslan <erdemarslan@ymail.com>
2019-02-26 15:05:05 +0300

    init

---
+++ index.js
@@ -0,0 +1,45 @@
+// @author Erdem Arslan
+// tiny smoothscroll plugin
+
+const smoothie = options => {
+    let isSmoothScrollSupported = 'scrollBehavior' in document.documentElement.style
+
+    if (!isSmoothScrollSupported) {
+        document.body.className += ' not-supported'
+    }
+
+    let options = {
+        target: arg.target || window,
+        behavior: arg.behavior,
+        top: arg.top,
+        left: arg.left
+    }
+
+    let api = {}
+
+    api.scrollBy = () => {
+        if (isSmoothScrollSupported) {
+            (options.target).scrollBy({
+                behavior: options.behavior,
+                top: options.top,
+                left: options.left
+            })
+        } else {
+            (options.target).scrollBy(options.left, options.top)
+        }
+    }
+
+    api.scrollTo = () => {
+        if (isSmoothScrollSupported) {
+            (options.target).scrollTo({
+                behavior: options.behavior,
+                top: options.top,
+                left: options.left
+            })
+        } else {
+            (options.target).scrollTo(options.left, options.top)
+        }
+    }
+
+    return api
+}
---
+++ readme.md
@@ -0,0 +1,25 @@
+## yet another smooth scroll thing.
+
+tiny (1KB), simple, bla bla plugin for smooth scroll stuffs.
+
+[Bon Appétit](https://www.youtube.com/watch?v=dPI-mRFEIH0)
+
+
+### Usage
+````javascript
+    // @usage
+    const _scroll = new smoothscroll({
+        behavior: 'smooth',
+        top: 0,
+        left: 0
+    })
+````
+
+### Calling
+````javascript
+    _scroll.scrollTo()
+
+    // or
+
+    _scroll.scrollBy()
+````
